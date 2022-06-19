22a9c2d659f59cde4875ca02fd9f2fab34202311
karroyo1204 <arroyok532@gmail.com>
2022-06-19 22:29:05 +0000

    Se agrego la connexion BD

---
+++ PHP/Conexion.php
@@ -0,0 +1,8 @@
+<?php
+
+$conn = oci_connect('hr', '12345', 'localhost/orcl');
+if (!$conn) {
+    $e = oci_error();
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+?>
