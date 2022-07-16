d53027d49102975835941ac7c5f6d0edf81a2e98
karroyo1204 <arroyok532@gmail.com>
2022-07-16 03:07:39 +0000

    Cambios en el header

---
+++ PHP/header.php
@@ -0,0 +1,11 @@
+<navbar>
+     <ul>
+        <li>Inicio</li>
+        <li>Acerca de nosotros</li>
+        <li>Menu</li>
+        <li>Ordenar express</li>
+        <li>Iniciar Sesion</li>
+    </ul>
+</navbar>
+
+        
